c8268afcfcaf4d03587f98d2dbbf430062a33515
Cedric Soual <cedric@cedrics-macbook-pro.home>
2022-08-21 00:01:02 +0200

    add swagger working example

--- src/main/java/org/example/infra/api/model/ComputeTaxRequest.java
+++ src/main/java/org/example/infra/api/model/ComputeTaxRequest.java
@@ -1,9 +1,17 @@
 package org.example.infra.api.model;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
 import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
 import java.util.List;
 
-public record ComputeTaxRequest(@NotNull String vehicleType, @NotNull List<OffsetDateTime> dates) {
+public record ComputeTaxRequest(
+        @NotNull
+        @Schema(required = true, example = "Car")
+        String vehicleType,
+        @NotNull
+        @Schema(required = true, example = "[\"2013-08-20T06:54:10.374Z\"]")
+        List<OffsetDateTime> dates) {
 
 }
